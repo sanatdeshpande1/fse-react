a252304bf4e5323637f8765b8a027773395d7fc1
Sanat Deshpande <sanatdeshpande1@gmail.com>
2022-03-27 21:35:13 -0400

    Likes button implemented

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({ tuit, likeTuit = () => { } }) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -14,12 +14,12 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                tuit.stats.likes > 0 &&
+                  <i className="fa fa-heart me-1" style={{color: 'red'}}></i>
               }
               {
-                tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-                  <i className="far fa-heart me-1"></i>
+                tuit.stats.likes <= 0 &&
+                  <i className="fa fa-heart me-1"></i>
               }
             {tuit.stats && tuit.stats.likes}
           </span>
